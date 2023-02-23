89cf1db9ef12bbee6692fbbdbd1e6f505877d3b3
Jai3699 <jaiyadav3699@gmail.com>
2023-02-23 20:37:59 +0530

    checking

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
 
 app = Flask(__name__)
 
@@ -23,19 +24,16 @@ def index():
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
+            #del bigboxes[0:3]
+            box = bigboxes[2]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
+            #print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
-            filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
+            
             reviews = []
             for commentbox in commentboxes:
                 try:
@@ -72,7 +70,15 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+            client = pymongo.MongoClient("mongodb+srv://jaiyadav3699:jaiyadav3699@cluster0.zr6n1sp.mongodb.net/?retryWrites=true&w=majority")
+            db=client['web_cmnt_dbse']
+            cmnt_collection=db['cmnt_collection']
+            cmnt_collection.insert_many(reviews)
+            return render_template('result.html', reviews=reviews)
+            
+           
+        
+
         except Exception as e:
             logging.info(e)
             return 'something is wrong'
